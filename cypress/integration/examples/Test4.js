1ab2e78909632191c9764b818ef67df843d59411
rmznkrc <rmznkrc11@gmail.com>
2023-04-10 13:03:05 -0700

    first commit

---
+++ cypress/integration/examples/Test4.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+ 
+describe('My First Test Suite', function() 
+{
+ 
+it('My FirstTest case',function() {
+ 
+ 
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+ 
+ cy.get
+
+
+}  )
+ 
+}  )
