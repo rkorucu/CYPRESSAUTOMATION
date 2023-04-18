8cbcb97cedf17c47f5259f1818192ea9ebe1c1cb
rmznkrc <rmznkrc11@gmail.com>
2023-04-18 03:57:00 -0700

    Framework is done

--- cypress/integration/examples/Test4.js
+++ cypress/integration/examples/Test4.js
@@ -1,13 +1,12 @@
 /// <reference types="Cypress" />
- 
-describe('My First Test Suite', function() 
+
+describe('My Second Test Suite', function() 
 {
- 
+
 it('My FirstTest case',function() {
- 
- 
-///Check boxes
-cy.visit("http://qaclickacademy.com/practice.php")
+
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 cy.get('#alertbtn').click()
 cy.get('[value="Confirm"]').click()
 //window:alert
@@ -16,21 +15,48 @@ cy.on('window:alert',(str)=>
     //Mocha
     expect(str).to.equal('Hello , share this practice page and share your knowledge')
 })
- 
+
 cy.on('window:confirm',(str)=>
 {
     //Mocha
     expect(str).to.equal('Hello , Are you sure you want to confirm?')
 })
- 
+
 cy.get('#opentab').invoke('removeAttr','target').click()
- 
+
 cy.url().should('include','qaclickacademy')
- 
+
 cy.go('back')
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }  )
- 
+
+
+
 }  )
